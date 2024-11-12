a93f7ef85a3f51a7dd92fd327fa3bce994272fcb
azkafr92 <azkafr92@gmail.com>
2024-11-13 05:37:35 +0700

    feat: port from env

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { LoggingInterceptor } from './logger/logger.interceptor';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableVersioning({ type: VersioningType.URI });
   app.setGlobalPrefix('api', { exclude: ['ping'] });
   app.useGlobalInterceptors(new LoggingInterceptor());
 
-  await app.listen(3000);
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
